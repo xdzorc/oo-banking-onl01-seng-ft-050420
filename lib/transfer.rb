fd60c7065d1f750d8cf31fab1d03428d5802d4b6
DAZUN XING <dazun.xing@gmail.com>
2020-05-23 03:21:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,4 +1,8 @@
 class Transfer
   # your code here
-  attr_accessor :
+  attr_accessor :sender, :receiver, :status, :amount
+  def initialize(sender, receiver, status="pending",amount)
+    @sender =sender 
+    @receiver =receiver
+  end
 end

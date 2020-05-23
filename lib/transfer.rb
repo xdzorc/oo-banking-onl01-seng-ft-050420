5bb8384cbe23334c618a3e5024e86f986e059ce3
DAZUN XING <dazun.xing@gmail.com>
2020-05-23 03:46:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
       @sender.balance -=@amount
       @receiver.balance +=@amount
       @status ="complete"
-    else
+    elsif @sender.balance < @amount
       @status ="rejected"
       "Transaction rejected. Please check your account balance."
     end

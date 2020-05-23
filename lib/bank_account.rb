6b6871bdfec8464acb1864529362c50de9830b0d
DAZUN XING <dazun.xing@gmail.com>
2020-05-23 03:07:40 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,5 +1,9 @@
 class BankAccount
-  attr_accessor :
+  attr_accessor :balance, :status
   attr_reader :name 
-  def initialize(name, balance=1000, status =)
+  def initialize(name, balance=1000, status ="open")
+    @name =name 
+    @balance =balance 
+    @status =status
+  end
 end
